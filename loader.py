4dd341ef58ea76f3099a2e2f62f624d2bd08c994
xinyiguan <xg792@nyu.edu>
2022-10-17 16:40:54 +0200

    work: loader updated

--- loader.py
+++ loader.py
@@ -1,9 +1,10 @@
 """
 The loader contains three class to read the
 """
+import fnmatch
 import os
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import List, Optional, Literal
 
 import pandas as pd
 
@@ -11,59 +12,34 @@ import pandas as pd
 @dataclass
 class PieceInfo:
     # assuming we are inside the corpus folder
-    parent_corpus_path:str
+    parent_corpus_path: str
     piece_name: str
 
     def __post_init__(self):
-        self.harmonies_df = self.get_harmonies_df(selected_keys=None)
-        self.measures_df = self.get_measures_df(selected_keys=None)
-        self.notes_df = self.get_notes_df(selected_keys=None)
+        self.corpus_name = self.parent_corpus_path.split(os.sep)[-2]
+        self.harmonies_df = self.get_aspect_df(aspect='harmonies', selected_keys=None)
+        self.measures_df = self.get_aspect_df(aspect='measures', selected_keys=None)
+        self.notes_df = self.get_aspect_df(aspect='notes', selected_keys=None)
 
-    def get_harmonies_df(self, selected_keys: Optional[List[str]]) -> pd.DataFrame:
+    def get_aspect_df(self, aspect: Literal['harmonies', 'measures', 'notes'],
+                      selected_keys: Optional[List[str]]) -> pd.DataFrame:
         """
-        To get the piece-wise harmonies tsv file as a DataFrame.
-        :param selected_keys: a list of keys (such as 'chord','numeral') in the tsv file. If none, return the entire df.
-        :return:
+        To get the piece-wise aspect(harmonies/measures/notes) tsv files as a DataFrame, always attach metadata (parent corpus, fname)
+        :param: selected_keys: a list of keys (such as 'chord','numeral') in the tsv file. If none, return the entire df.
+        :return: a DataFrame
         """
-        piece_harmonies_tsv_path = self.parent_corpus_path+'harmonies/' + self.piece_name + '.tsv'
-        all_df = pd.read_csv(piece_harmonies_tsv_path, sep='\t')
+
+        piece_aspect_tsv_path = self.parent_corpus_path + aspect + '/' + self.piece_name + '.tsv'
+        all_df = pd.read_csv(piece_aspect_tsv_path, sep='\t')
+        df_length = all_df.shape[0]
+        all_df['corpus'] = [self.corpus_name] * df_length
+        all_df['fname'] = [self.piece_name] * df_length
 
         if selected_keys is None:
             return all_df
         else:
-            selected_df = all_df[selected_keys]
+            selected_df = all_df[selected_keys].copy()
             return selected_df
-
-    def get_measures_df(self, selected_keys: Optional[List[str]]) -> pd.DataFrame:
-        """
-        To get the piece-wise measures tsv file as a DataFrame.
-        :param selected_keys: a list of keys in the tsv file. If none, return the entire df.
-        :return:
-        """
-        piece_measure_tsv_path = self.parent_corpus_path+'meausres/' + self.piece_name + '.tsv'
-        all_df = pd.read_csv(piece_measure_tsv_path, sep='\t')
-
-        if selected_keys is None:
-            return all_df
-        else:
-            selected_df = all_df[selected_keys]
-            return selected_df
-
-    def get_notes_df(self, selected_keys: Optional[List[str]]) -> pd.DataFrame:
-        """
-        To get the piece-wise harmonies tsv file as a DataFrame.
-        :param selected_keys: a list of keys (such as 'chord','numeral') in the tsv file. If none, return the entire df.
-        :return:
-        """
-        piece_notes_tsv_path = self.parent_corpus_path+'notes/' + self.piece_name + '.tsv'
-        all_df = pd.read_csv(piece_notes_tsv_path, sep='\t')
-
-        if selected_keys is None:
-            return all_df
-        else:
-            selected_df = all_df[selected_keys]
-            return selected_df
-
 
 
 @dataclass
@@ -71,10 +47,15 @@ class CorpusInfo:
     corpus_path: str
 
     def __post_init__(self):
+        self.corpus_name = self.corpus_path.split(os.sep)[-2]
+        self.piece_name_list = sorted([f.replace('.tsv', '') for f in os.listdir(self.corpus_path + 'harmonies/')
+                                       if not f.startswith('.')
+                                       if not f.startswith('__')])
         self.metadata_df = self._get_metadata()
-        self.concat_harmonies_df = self._get_concat_harmonies_df()
-
-
+        self.corpus_harmonies_df = self.get_corpus_aspect_df(aspect='harmonies', selected_keys=None)
+        self.corpus_measures_df = self.get_corpus_aspect_df(aspect='measures', selected_keys=None)
+        self.corpus_notes_df = self.get_corpus_aspect_df(aspect='notes', selected_keys=None)
+        self.is_annotated = self._check_annotated()
 
     def _get_metadata(self) -> pd.DataFrame:
         """
@@ -83,15 +64,73 @@ class CorpusInfo:
 
         metadata_tsv_path = self.corpus_path + 'metadata.tsv'
         metadata_df = pd.read_csv(metadata_tsv_path, sep='\t')
-        corpus_name = self.corpus_path.split(os.sep)[-2]  # ['dcml_corpora', 'debussy_suite_bergamasque', '']
-        corpus_name_list = [corpus_name] * len(metadata_df)
+        corpus_name_list = [self.corpus_name] * len(metadata_df)
         metadata_df['corpus'] = corpus_name_list
 
         return metadata_df
 
-    def _get_concat_harmonies_df(self):
-        raise NotImplementedError
+    def get_corpus_aspect_df(self, aspect: Literal['harmonies', 'measures', 'notes'],
+                             selected_keys: Optional[List[str]]) -> pd.DataFrame:
+        """
+        To concat all the sub-pieces info dataframe into a large one.
+        For each piece, always attach metadata:  annotated_key, ambitus, composed_end, composed_start
+        :param selected_keys:
+        :param aspect: Literal['harmonies', 'measures', 'notes']
+        :return: DataFrame
+        """
 
+        concat_df_list = []
+        for idx, val in enumerate(self.piece_name_list):
+            piece = PieceInfo(parent_corpus_path=self.corpus_path, piece_name=val)
+            aspect_all_df = piece.get_aspect_df(aspect=aspect, selected_keys=None)
+
+            df_length = aspect_all_df.shape[0]
+
+            # get the row index of metadata with matching fnames:
+            correpond_piece_row_idx_in_metadata_df = \
+                self.metadata_df.index[self.metadata_df['fnames'] == val].to_list()[0]
+
+            aspect_all_df['annotated_key'] = [self.metadata_df['annotated_key'][
+                                                  correpond_piece_row_idx_in_metadata_df]] * df_length
+            aspect_all_df['ambitus'] = [self.metadata_df['ambitus'][correpond_piece_row_idx_in_metadata_df]] * df_length
+            aspect_all_df['composed_end'] = [self.metadata_df['composed_end'][
+                                                 correpond_piece_row_idx_in_metadata_df]] * df_length
+            aspect_all_df['composed_start'] = [self.metadata_df['composed_start'][
+                                                   correpond_piece_row_idx_in_metadata_df]] * df_length
+            concat_df_list.append(aspect_all_df)
+        concat_aspect_df = pd.concat(concat_df_list)
+
+        if selected_keys is None:
+            return concat_aspect_df
+        else:
+            selected_aspect_df = concat_aspect_df[selected_keys].copy()
+            return selected_aspect_df
+
+    def get_corpuswise_unique_key_values(self, aspect: Literal['harmonies', 'measures', 'notes'],
+                                         key: str) -> List[str]:
+        if aspect == 'harmonies':
+            df = self.corpus_harmonies_df
+            unique_key_vals = df[key].unique().tolist()
+            return unique_key_vals
+
+        elif aspect == 'measures':
+            df = self.corpus_measures_df
+            unique_key_vals = df[key].unique().tolist()
+            return unique_key_vals
+
+        elif aspect == 'notes':
+            df = self.corpus_notes_df
+            unique_key_vals = df[key].unique().tolist()
+            return unique_key_vals
+
+    def _check_annotated(self) -> bool:
+        conditions = [
+            any(file for file in os.listdir(self.corpus_path + 'harmonies/') if file.endswith('.tsv')),
+            any(file for file in os.listdir(self.corpus_path + 'measures/') if file.endswith('.tsv')),
+            any(file for file in os.listdir(self.corpus_path + 'notes/') if file.endswith('.tsv')),
+        ]
+        is_annotated = all(conditions)
+        return is_annotated
 
 
 @dataclass
@@ -99,33 +138,35 @@ class MetaCorpraInfo:
     meta_corpora_path: str
 
     def __post_init__(self):
-        self.corpus_list = [f for f in os.listdir(self.meta_corpora_path)
-                            if not f.startswith('.')
-                            if not f.startswith('__')]
-        self.corpus_paths = [self.meta_corpora_path + val + '/' for idx, val in enumerate(self.corpus_list)]
-        self.annotated_corpus_list = self._get_annotated_corpus_list()
-        self.annotated_corpus_paths = [self.meta_corpora_path + val + '/' for idx, val in
-                                       enumerate(self.annotated_corpus_list)]
+        self.corpus_name_list: List[str] = sorted([f for f in os.listdir(self.meta_corpora_path)
+                                        if not f.startswith('.')
+                                        if not f.startswith('__')])
+        self.corpus_paths:List[str] = [self.meta_corpora_path + val + '/' for idx, val in enumerate(self.corpus_name_list)]
+        self.corpus_list: List[CorpusInfo] = [CorpusInfo(corpus_path=path) for path in self.corpus_paths]
+        self.annotated_corpus_list: List[CorpusInfo] = [corpus_info for corpus_info in self.corpus_list if
+                                                        corpus_info.is_annotated]
 
-    def _get_annotated_corpus_list(self) -> List[str]:
-        """
-        a band-aid method to get the annotated corpus list
-        (check if tsv files in harmonies/ and measures/ and notes/ exist)
-        """
+    def get_corpora_aspect_df(self, aspect: Literal['harmonies', 'measures', 'notes'],
+                             selected_keys: Optional[List[str]], annotated: bool = True) -> pd.DataFrame:
+        if annotated is True:
+            concat_df_list = []
+            for idx, val in enumerate(self.annotated_corpus_list):
+                aspect_all_df = val.get_corpus_aspect_df(aspect=aspect, selected_keys=selected_keys)
+                concat_df_list.append(aspect_all_df)
+            corpora_aspect_df = pd.concat(concat_df_list)
+            return corpora_aspect_df
+        else:
+            concat_df_list = []
+            for idx, val in enumerate(self.corpus_list):
+                aspect_all_df = val.get_corpus_aspect_df(aspect=aspect, selected_keys=selected_keys)
+                concat_df_list.append(aspect_all_df)
+            corpora_aspect_df = pd.concat(concat_df_list)
+            return corpora_aspect_df
 
-        raise NotImplementedError
-
-    def get_corpora_harmonies_df(self, contain_metadata: bool = True):
-        raise NotImplementedError
-
-    def get_corpora_measures_df(self, contain_metadata: bool = True):
-        raise NotImplementedError
-
-    def get_corpora_notes_df(self, contain_metadata: bool = True):
-        raise NotImplementedError
 
 
 if __name__ == '__main__':
-    path = 'dcml_corpora/debussy_suite_bergamasque/'
-    piece = PieceInfo(parent_corpus_path=path, piece_name='l075-01_suite_prelude')
-    print(piece.get_harmonies_df(selected_keys=['chord', 'numeral']))
+    metacorpora_path='romantic_piano_corpus/'
+    metacorpora= MetaCorpraInfo(metacorpora_path)
+    df=metacorpora.get_corpora_aspect_df(aspect='harmonies', selected_keys=['chord', 'numeral', 'fname', 'corpus'], annotated=True)
+    print(df)
