502ad1be00bdd1b7247a28c67b442dc3b03bea50
xinyiguan <xg792@gmail.com>
2022-10-20 22:39:14 +0200

    home: n-gram (transition matrix) -v3

--- n_gram.py
+++ n_gram.py
@@ -7,9 +7,13 @@ import pandas as pd
 import loader
 
 
-def get_transition_matrix(sequence: List[str], n=1) -> pd.DataFrame:
+def get_transitions(sequence: List[str], n=1) -> np.ndarray:
     transitions = np.array(
         [['_'.join(sequence[i - n:i]), sequence[i]] for i in range(n, len(sequence))])
+    return transitions
+
+
+def get_transition_matrix(transitions: np.ndarray) -> pd.DataFrame:
     contexts, targets = np.unique(transitions[:, 0]), np.unique(transitions[:, 1])
     transition_matrix = pd.DataFrame(0, columns=targets, index=contexts)
     for i, transition in enumerate(transitions):
@@ -41,15 +45,21 @@ class N_Gram:
 
 
 if __name__ == '__main__':
-    piece = loader.PieceInfo(parent_corpus_path='romantic_piano_corpus/debussy_suite_bergamasque/',
-                             piece_name='l075-01_suite_prelude')
-    harmonies = piece.get_aspect_df('harmonies', selected_keys=['numeral']).values.flatten().tolist()
+    piece1 = loader.PieceInfo(parent_corpus_path='romantic_piano_corpus/debussy_suite_bergamasque/',
+                              piece_name='l075-01_suite_prelude')
+    piece2 = loader.PieceInfo(parent_corpus_path='romantic_piano_corpus/debussy_suite_bergamasque/',
+                              piece_name='l075-02_suite_menuet')
+    pieces = [piece1, piece2]
+    corpera_transitions = []
+    for piece in pieces:
+        harmonies = piece.get_aspect_df('harmonies', selected_keys=['numeral']).values.flatten().tolist()
+        transitions = get_transitions(sequence=harmonies, n=2)
+        corpera_transitions.extend(transitions)
     # unigram = piece.get_aspect_df(aspect='harmonies', selected_keys=['numeral']).values.flatten().tolist()
     # n_gram = N_Gram(piece=piece, aspect='harmonies', key='numeral', n=2)
     # print(len(n_gram.get_grams_seq()))
 
-    sequence = harmonies
-    transition_matrix = get_transition_matrix(sequence, n=1)
+    transition_matrix = get_transition_matrix(transitions)
     transition_probs = transition_matrix.divide(transition_matrix.sum(axis=1), axis=0)
     print(transition_matrix)
     print(transition_probs)
