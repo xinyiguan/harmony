2255fe2e6e230da4b5f4b32b393a643f1e5d6292
xinyiguan <xg792@nyu.edu>
2022-11-11 17:12:30 +0100

    work: RomanNumeral class refactoring

---
+++ delete_git_files.py
@@ -0,0 +1,32 @@
+# Created by Xinyi Guan in 2022.
+import os
+import shutil
+
+
+def delete_files():
+
+    folder_path = 'dcml_corpora/'
+
+    for corpus in os.listdir(folder_path):
+        corpus_path = folder_path+corpus+'/'
+        print(corpus_path)
+        try:
+            # remove .github/
+            shutil.rmtree(corpus_path+'.github/')
+        except:
+            print('No such file/folder.')
+            # remove .git/
+        try:
+            shutil.rmtree(corpus_path+'.git/')
+        except:
+            print('No such file/folder.')
+            # remove .git
+        try:
+            shutil.rmtree(corpus_path+'.git')
+        except:
+            print('No such file/folder.')
+
+
+
+if __name__=='__main__':
+    delete_files()
---
+++ plot_dimcat.py
@@ -0,0 +1,5 @@
+# Created by Xinyi Guan in 2022.
+
+import dimcat, pitchtypes
+import pandas as pd
+
