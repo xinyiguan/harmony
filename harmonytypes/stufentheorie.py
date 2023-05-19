61aaa36e84037acd8327a830081bce93b2dd79f6
xinyiguan <xg792@nyu.edu>
2023-05-19 17:45:50 +0200

    [lab] Stufen class update

--- harmonytypes/stufentheorie.py
+++ harmonytypes/stufentheorie.py
@@ -1,9 +1,11 @@
 # defining concepts in Schenkerian theory
 import re
+from collections import namedtuple
 from dataclasses import dataclass
 from typing import Literal, List, TypeVar, Generic, Self
 
 import pandas as pd
+import regex_spm
 from pitchtypes import SpelledPitchClass
 
 # HARD ENCODING =====================================================================================================
@@ -26,7 +28,6 @@ combined_major_minor_system_dict = {"I": 1, "i": 1,
                                     "vi": 6, "bVI": 6, "VI": 6, "bvi": 6,
                                     "viio": 7, "bVII": 7, "VII": 7, "bvii": 7}
 
-
 simple_mixture_dict = {"I": ["i"], "ii": ["bII", "iio"], "iii": ["bIII"], "IV": ["iv"],
                        "V": ["v"], "vi": ["bVI"], "viio": ["bVII"],
 
@@ -46,39 +47,43 @@ double_mixture_dict = {"I": ["i"], "ii": ["bii", "iio"], "iii": ["biii"], "IV": 
                        "v": ["V"], "bVI": ["VI"], "bVII": ["vii", "VII"]}
 
 
-
 @dataclass
-class Triad: # Schenker Harmony section III, chap 1, p. 182
+class Triad:  # Schenker Harmony section III, chap 1, p. 182
     pass
 
+
 @dataclass
-class Stufe:
-    stufen_str: str
+class AbstractStufe:
+    stufe_str: str
+
+    """
+    A Stufe (scale step) is notated in uppercase Roman numeral, defined by its position to tonic
+    """
 
     @classmethod
-    def from_string(cls, stufen_str: str) -> Self:
-        stufen_regex = re.compile("^(?P<alteration>(b*)|(#*))"
-                                  "(?P<numeral>(I|i|II|ii|III|iii|IV|iv|V|v|VI|vi|VII|vii))"
-                                  "(?P<diminshed>(o*))$")
+    def from_string(cls, stufe_str: str) -> Self:
+
+        abstract_stufen_regex = re.compile("^(?P<numeral>(I|II|III|IV|V|VI|VII))$")
 
         # check input string regex:
-        if not isinstance(stufen_str, str):
-            raise TypeError(f"expected string as input, got {stufen_str}")
-        match = stufen_regex.fullmatch(stufen_str)
+        match = abstract_stufen_regex.fullmatch(stufe_str)
         if match is None:
-            raise ValueError(f"could not match '{stufen_str}' with regex: '{stufen_regex.pattern}'")
+            raise ValueError(f"could not match '{stufe_str}' with regex: '{abstract_stufen_regex.pattern}'")
 
-        instance = cls(stufen_str=stufen_str)
+        instance = cls(stufe_str=stufe_str)
         return instance
+
+    def possible_forms(self, diatonic_mode: Literal["major", "minor"]) -> List[str]:
+        raise NotImplementedError
 
     def simple_mixture(self) -> List[Self]:
         """Transform the current stufe to another via simple mixture"""
-        if self.stufen_str in major_diatonic_system_dict:
-            value = major_diatonic_system_dict.get(self.stufen_str)
+        if self.stufe_str in major_diatonic_system_dict:
+            value = major_diatonic_system_dict.get(self.stufe_str)
             resulting_stufen = [Stufe.from_string(k) for k, v in minor_diatonic_system_dict.items() if v == value]
 
-        elif self.stufen_str in minor_diatonic_system_dict:
-            value = minor_diatonic_system_dict.get(self.stufen_str)
+        elif self.stufe_str in minor_diatonic_system_dict:
+            value = minor_diatonic_system_dict.get(self.stufe_str)
             resulting_stufen = [Stufe.from_string(k) for k, v in major_diatonic_system_dict.items() if v == value]
 
         else:
@@ -87,12 +92,12 @@ class Stufe:
         return resulting_stufen
 
     def secondary_mixture(self) -> List[Self]:
-        if self.stufen_str in major_diatonic_system_dict:
-            value = major_secondary_system_dict.get(self.stufen_str)
+        if self.stufe_str in major_diatonic_system_dict:
+            value = major_secondary_system_dict.get(self.stufe_str)
             resulting_stufen = [Stufe.from_string(k) for k, v in minor_secondary_system_dict.items() if v == value]
 
-        elif self.stufen_str in minor_diatonic_system_dict:
-            value = minor_secondary_system_dict.get(self.stufen_str)
+        elif self.stufe_str in minor_diatonic_system_dict:
+            value = minor_secondary_system_dict.get(self.stufe_str)
             resulting_stufen = [Stufe.from_string(k) for k, v in major_secondary_system_dict.items() if v == value]
 
         else:
@@ -101,15 +106,47 @@ class Stufe:
         return resulting_stufen
 
     def double_mixture(self) -> Self:
-        if self.stufen_str in major_diatonic_system_dict:
-            value = major_secondary_system_dict.get(self.stufen_str)
+        if self.stufe_str in major_diatonic_system_dict:
+            value = major_secondary_system_dict.get(self.stufe_str)
             resulting_stufen = [Stufe.from_string(k) for k, v in minor_secondary_system_dict.items() if v == value]
 
-        elif self.stufen_str in minor_diatonic_system_dict:
-            value = minor_secondary_system_dict.get(self.stufen_str)
+        elif self.stufe_str in minor_diatonic_system_dict:
+            value = minor_secondary_system_dict.get(self.stufe_str)
             resulting_stufen = [Stufe.from_string(k) for k, v in major_secondary_system_dict.items() if v == value]
 
         else:
             raise ValueError
 
         return resulting_stufen
+
+
+@dataclass
+class Stufe:
+    stufe_str: str
+    abstract_stufe: AbstractStufe
+    quality: Literal["major", "minor", "diminished"]
+
+    @classmethod
+    def from_string(cls, stufe_str: str) -> Self:
+        stufen_regex = re.compile("^(?P<alteration>(b*)|(#*))"
+                                  "(?P<numeral>(I|i|II|ii|III|iii|IV|iv|V|v|VI|vi|VII|vii))"
+                                  "(?P<diminished>(o*))$")
+
+        # check input string regex:
+        matching = stufen_regex.fullmatch(stufe_str)
+        if matching is None:
+            raise ValueError(f"could not match '{stufe_str}' with regex: '{stufen_regex.pattern}'")
+
+        stufe_str = stufe_str
+        abstract_stufe = AbstractStufe.from_string(stufe_str=matching["numeral"].upper())
+
+        qualtiy = ...  # TODO
+
+        instance = cls(stufe_str=stufe_str, abstract_stufe=abstract_stufe, quality=quality)
+
+        return instance
+
+
+if __name__ == "__main__":
+    result = Stufe.from_string(stufe_str="bII")
+    print(f'{result=}')
